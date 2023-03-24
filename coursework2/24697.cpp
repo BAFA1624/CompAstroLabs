4fbf81f46610c20c2bcc350f597af3f4be67e916
Ben Andrews <benafandrews@gmail.com>
2023-03-24 16:35:41 +0000

    Got weighted distribution working correctly this time, working on 1.b.

--- coursework2/24697.cpp
+++ coursework2/24697.cpp
@@ -1,8 +1,12 @@
+#include <algorithm>
 #include <cmath>
 #include <fstream>
 #include <functional>
+#include <initializer_list>
 #include <iostream>
+#include <numeric>
 #include <vector>
+
 
 enum class distr_type { flat, rejection, weighted };
 
@@ -22,6 +26,17 @@ class distribution
         m_rmax(),
         m_f(),
         m_endpoint(){};
+
+    F random_sample() noexcept {
+        m_seed = ( static_cast<std::uint64_t>( 16807 ) * m_seed )
+                 % static_cast<std::uint64_t>( 2147483647 );
+        return static_cast<F>( m_seed ) / static_cast<F>( 2147483647 );
+    }
+
+    F random() const noexcept {
+        const F x{ ( m_dmax - m_dmin ) * random_sample() + m_dmin };
+        return transform( x );
+    }
 
     F transform( const F x ) const noexcept;
 
@@ -48,11 +63,7 @@ class distribution
             return 1.;
         },
         const bool include_endpoint = true ) noexcept;
-    F random_sample() noexcept {
-        m_seed = ( static_cast<uint64_t>( 16807 ) * m_seed )
-                 % static_cast<std::uint64_t>( 2147483647 );
-        return static_cast<F>( m_seed ) / static_cast<F>( 2147483647 );
-    }
+
 
     distr_type                 m_type;      // Type of distribution
     std::int64_t               m_seed;      // Seed value
@@ -126,18 +137,11 @@ distribution<F>::transform( const std::vector<F> & v ) const noexcept {
 template <std::floating_point F>
 F
 distribution<F>::transform_nonweighted( const F x ) const noexcept {
-    const F dx{ 1 / static_cast<F>( m_M ) };
     const F xval{ ( x - m_dmin ) / ( m_dmax - m_dmin ) };
-
-    std::cout << "transform_nonweighted (const F " << x << " ) -> F "
-              << ( m_bin_count[static_cast<std::uint64_t>( xval / dx )] * m_M
-                   / ( ( m_dmax - m_dmin ) * static_cast<F>( m_N ) ) )
-                     + m_rmin
-              << std::endl;
-
-    return ( m_bin_count[static_cast<std::uint64_t>( xval / dx )] * m_M
-             / ( ( m_dmax - m_dmin ) * static_cast<F>( m_N ) ) )
-           + m_rmin;
+    return ( ( m_bin_count[static_cast<std::uint64_t>(
+                   xval * static_cast<F>( m_M ) )]
+               * m_M / ( ( m_dmax - m_dmin ) * static_cast<F>( m_N ) ) )
+             + m_rmin );
 }
 
 template <std::floating_point F>
@@ -145,12 +149,8 @@ F
 distribution<F>::transform_weighted( const F x ) const noexcept {
     const F    xval{ ( x - m_dmin ) / ( m_dmax - m_dmin ) };
     const auto i{ static_cast<std::uint64_t>( xval * m_M ) };
-    const F p_flat{ m_bin_count[i] * m_M / ( m_rmax * static_cast<F>( m_N ) ) };
-
-    std::cout << "transform_weighted (const F " << x << " ) -> F "
-              << p_flat * m_weights[i] << std::endl;
-
-    return p_flat * m_weights[i];
+    return m_weights[i] * m_bin_count[i] * m_M
+           / ( m_rmax * static_cast<F>( m_N ) );
 }
 
 template <std::floating_point F>
@@ -181,7 +181,6 @@ distribution<F>::gen_distribution( const distr_type    type,
         }
     } break;
     case distr_type::rejection: {
-        std::cout << "rejection method" << std::endl;
         for ( std::uint64_t i{ 0 }; i < m_N; ++i ) {
             F x{ 0 }, y{ 0 };
             do {
@@ -205,29 +204,184 @@ distribution<F>::gen_distribution( const distr_type    type,
                 x * static_cast<F>( m_M ) )]++;
         } );
 
+        // Draw N new samples
+        std::vector<F> new_samples( m_N, F{ 0 } );
+        std::generate( new_samples.begin(), new_samples.end(),
+                       [this]() { return random_sample(); } );
+
         // Calculate weights in two passes
         // 1) For each sample, weight bin index = same as in flat
         // distribution,
         //    add P(x) / Q(x) to bin
-        std::for_each( samples.cbegin(), samples.cend(), [this]( const F x ) {
-            const auto i = static_cast<std::uint64_t>(
-                static_cast<std::uint64_t>( x * static_cast<F>( m_M ) ) );
-            const F qi{ m_bin_count[i] * m_M
-                        / ( m_rmax * static_cast<F>( m_N ) ) };
-            const F pi{ m_f( ( m_dmax - m_dmin ) * x + m_dmin ) };
-
-            m_weights[i] += pi / qi;
-        } );
+        for ( const auto & s : new_samples ) {
+            const auto i{ static_cast<std::uint64_t>(
+                s * static_cast<F>( m_M ) ) };
+            const F    xval = ( m_dmax - m_dmin ) * s + m_dmin;
+            const F    p_flat{ m_bin_count[i] * m_M / ( ( m_rmax ) *m_N ) };
+            m_weights[i] += ( m_f( xval ) / p_flat );
+        }
         // 2) For each bin in both the flat distribution and the weights,
         // divide the weight sum by the number of samples in the bin.
-        for ( std::size_t i = 0; i < m_M; ++i ) {
+        for ( std::uint64_t i{ 0 }; i < m_M; ++i ) {
             m_weights[i] /= m_bin_count[i];
         }
-    } break;
+
+        break;
     }
 
-    return;
+        return;
+    }
 }
+
+
+enum class angle_change_type { add, random };
+
+template <std::floating_point F>
+class photon
+{
+    public:
+    photon( const angle_change_type change_type, const F x, const F y,
+            const F z, const F theta, const F phi ) :
+        m_change_type( change_type ),
+        m_x( x ),
+        m_y( y ),
+        m_z( z ),
+        m_theta( theta ),
+        m_phi( phi ),
+        m_absorbed( false ) {}
+
+    photon & move( const F tau = 0 ) noexcept;
+    photon & scatter( const F theta = 0, const F phi = 0 ) noexcept;
+    // photon & scatter_add( const F theta, const F phi ) noexcept;
+    // photon & scatter_random( const F theta, const F phi ) noexcept;
+
+    [[nodiscard]] std::tuple<F, F, F> pos() const noexcept {
+        return { m_x, m_y, m_z };
+    }
+    [[nodiscard]] std::tuple<F, F> dir() const noexcept {
+        return { m_theta, m_phi };
+    }
+    [[nodiscard]] F x() const noexcept { return m_x; }
+    [[nodiscard]] F y() const noexcept { return m_y; }
+    [[nodiscard]] F z() const noexcept { return m_z; }
+    [[nodiscard]] F theta() const noexcept { return m_theta; }
+    [[nodiscard]] F phi() const noexcept { return m_phi; }
+    [[nodiscard]] F absorbed() const noexcept { return m_absorbed; }
+    void            set_absorbed() noexcept { m_absorbed = true; }
+
+    private:
+    F m_x;
+    F m_y;
+    F m_z;
+
+    F m_theta;
+    F m_phi;
+
+    bool m_absorbed;
+
+    angle_change_type m_change_type;
+};
+
+template <std::floating_point F>
+photon<F> &
+photon<F>::move( const F tau ) noexcept {
+    const F s_theta{ std::sin( m_theta ) }, c_theta{ std::cos( m_theta ) },
+        s_phi{ std::sin( m_phi ) }, c_phi{ std::cos( m_phi ) };
+    m_x += tau * s_theta * c_phi;
+    m_y += tau * s_theta * s_phi;
+    m_z += tau * c_theta;
+
+    return *this;
+}
+
+template <std::floating_point F>
+photon<F> &
+photon<F>::scatter( const F theta, const F phi ) noexcept {
+    constexpr if ( m_change_type == angle_change_type::random ) {
+        m_theta = theta;
+        m_phi = phi;
+    }
+    else {
+        const F tmp_theta = m_theta + theta;
+        m_theta = tmp_theta + std::floor( tmp_theta / M_PI ) * ( -2 * theta );
+        m_phi = ( m_phi + phi ) % ( 2 * M_PI );
+    }
+    // std::cout << "theta: " << m_theta << std::endl;
+    // std::cout << "phi: " << m_phi << std::endl;
+    return *this;
+}
+
+template <std::floating_point F>
+photon<F>
+track_photon( photon<F> & p, const distribution<F> & d_theta,
+              const distribution<F> & d_phi, const distribution<F> & d_albedo,
+              const F tau = 10, const F albedo = 1., const F zmin = 0.,
+              const F zmax = 1. ) {
+    goto LOOP_START;
+
+REGENERATE_PHOTON:
+    p = photon<F>{ type, 0, 0, 0, 0, 0 };
+    goto LOOP_START;
+
+    do {
+        // Scatter or absorb?
+        if ( albedo.random_sample() >= a ) {
+            // Absorb photon
+            std::cout << "Photon absorbed, this is shouldn't be reached."
+                      << std::endl;
+            p.set_absorbed();
+            goto END;
+        }
+
+        // Scatter
+        p.scatter( theta.random(), phi.random() );
+
+        // Move
+    LOOP_START:
+        p.move( tau );
+    } while ( p.z() >= zmin && p.z() <= zmax );
+
+    if ( z < zmin ) {
+        goto REGENERATE_PHOTON;
+    }
+
+END:
+    return p;
+}
+
+template <std::floating_point F>
+void
+isotropic_scattering( const std::uint64_t N, const angle_change_type type,
+                      const F tau = 10, const F albedo = 1, const F zmin = 0.,
+                      const F zmax = 0 ) {
+    distribution<F> d_theta;
+    distribution<F> d_phi;
+    distribution<F> d_albedo;
+    // Initialize theta & phi distributions with some random seed values
+    d_theta.gen_distribution( distr_type::flat, 12341235, 1000000, 1000, 0, 1,
+                              0, M_PI );
+    d_phi.gen_distribution( distr_type::flat, 5439867, 1000000, 1000, 0, 1, 0,
+                            M_PI );
+    // Albedo distribution doesn't need high N, the distribution needs to vary
+    // from 0 - 1 so just use result of distribution::random_sample() instead.
+    d_albedo.gen_distribution( distr_type::flat, 1, 1, 1, 0, 1, 0, 1 );
+
+    // Initialize N photons at position (0, 0, 0) and angle (0, 0)
+    std::vector<photon<F>> photons( N, photon<F>{ type, 0, 0, 0, 0, 0 } );
+
+    // Launch particles
+    std::for_each( photons.begin(), photons.end(), [&]( const photon<F> & p ) {
+        return track_photon( p, d_theta, d_phi, d_albedo, tau, albedo, zmin,
+                             zmax );
+    } );
+
+    // Bin by intensity
+    const F d_mu{ 2 * M_PI / 10 };
+}
+
+void
+thomson_scattering() {}
+
 
 template <typename T>
 std::vector<T>
@@ -250,57 +404,14 @@ linspace( const T & start, const T & end, const std::uint64_t N,
     return result;
 }
 
-template <typename T,
-          typename T_it = decltype( std::begin( std::declval<T>() ) )>
-constexpr auto
-enumerate( const T & iterable ) {
-    struct enum_it
-    {
-        enum_it( const std::size_t i, const T_it it ) : m_i( i ), m_it( it ) {}
-
-        [[nodiscard]] bool operator!=( const enum_it & other ) const noexcept {
-            return m_it != other.m_it;
-        }
-        void operator++() noexcept {
-            ++m_i;
-            ++m_it;
-        }
-        auto operator*() const { return std::tie( m_i, *m_it ); }
-        auto operator*() { return std::tie( m_i, *m_it ); }
-
-        std::size_t m_i;
-        T_it        m_it;
-    };
-    struct enum_it_wrapper
-    {
-        T m_it;
-
-        enum_it_wrapper( const T & it ) : m_it( it ) {}
-
-        [[nodiscard]] auto begin() const noexcept {
-            return enum_it( 0, std::begin( m_it ) );
-        }
-        [[nodiscard]] auto cbegin() const {
-            return enum_it( 0, std::cbegin( m_it ) );
-        }
-        [[nodiscard]] auto end() const noexcept {
-            return enum_it{ 0, std::end( m_it ) };
-        }
-        [[nodiscard]] auto cend() const noexcept {
-            return enum_it{ 0, std::cend( m_it ) };
-        }
-    };
-    return enum_it_wrapper( iterable );
-}
-
 template <std::floating_point F>
 void
 write_to_file( const std::string_view fname, const std::vector<F> & x,
-               const std::vector<F> & y ) {
+               const std::vector<F> & y, const auto precision ) {
     const std::uint64_t n_rows{ std::min( x.size(), y.size() ) };
     std::ofstream       f( fname );
     for ( std::uint64_t i{ 0 }; i < n_rows; ++i ) {
-        f << x[i] << "," << y[i] << "\n";
+        f << std::setprecision( precision ) << x[i] << "," << y[i] << "\n";
     }
 }
 
@@ -316,33 +427,36 @@ p_mu( F m ) {
     return 0.375 * ( 1 + pow( m, 2.0 ) );
 }
 
+template <std::floating_point F>
+F
+p_theta( F theta ) {
+    return 0.375 * ( 1 + pow( cos( theta ), 2.0 ) ) * sin( theta );
+}
+
 int
 main() {
     const std::uint64_t N{ 1000000 };
     const std::uint64_t M{ 1000 };
 
-    distribution<double> sq_sine{};
-    sq_sine.gen_distribution(
-        distr_type::weighted, static_cast<std::uint64_t>( 1 ), N, M,
-        static_cast<double>( 0 ), static_cast<double>( M_PI ),
-        static_cast<double>( 0. ), static_cast<double>( M_2_PI ),
-        []( const double x ) -> double {
-            return M_2_PI * powl( sinl( x ), 2 );
-        } );
+    const double dmin{ -1 }, dmax{ 1 }, rmin{ 0. }, rmax{ 0.75 };
 
-    const auto xvals1 = linspace<double>( 0, M_PI, M, false );
-    const auto yvals1{ sq_sine.transform( xvals1 ) };
+    distribution<double> rejection{};
+    rejection.gen_distribution( distr_type::rejection,
+                                static_cast<std::uint64_t>( 1 ), N, M, dmin,
+                                dmax, rmin, rmax, p_mu<double> );
 
-    write_to_file( "data1.csv", xvals1, yvals1 );
+    const auto xvals1 = linspace<double>( dmin, dmax, M, false );
+    const auto yvals1{ rejection.transform( xvals1 ) };
 
-    distribution<double> distr{};
-    distr.gen_distribution(
-        distr_type::weighted, static_cast<std::uint64_t>( 1 ), N, M,
-        static_cast<double>( -1 ), static_cast<double>( 1 ),
-        static_cast<double>( 0 ), static_cast<double>( 0.75 ), p_mu<double> );
+    write_to_file( "data1.csv", xvals1, yvals1, 20 );
 
-    const auto xvals2 = linspace<double>( -1, 1, M, false );
-    const auto yvals2{ distr.transform( xvals2 ) };
+    distribution<double> weighted{};
+    weighted.gen_distribution( distr_type::weighted,
+                               static_cast<std::uint64_t>( 1 ), N, M, dmin,
+                               dmax, rmin, rmax, p_mu<double> );
 
-    write_to_file( "data2.csv", xvals2, yvals2 );
+    const auto xvals2 = linspace<double>( dmin, dmax, M, false );
+    const auto yvals2{ weighted.transform( xvals2 ) };
+
+    write_to_file( "data2.csv", xvals2, yvals2, 20 );
 }
--- coursework2/plot.py
+++ coursework2/plot.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from scipy.optimize import curve_fit
 
 
 mpl.rcParams["mathtext.fontset"] = "cm"
@@ -21,12 +23,12 @@ mu_vals = mu(theta_vals)
 p_vals = p(np.linspace(-1, 1, 10000))
 
 
-fig = plt.figure(figsize=(8, 8))
+fig1 = plt.figure(figsize=(8, 8))
 
-fig.suptitle(
+fig1.suptitle(
     r"Expected Probability Distribution, $-\pi \leq \theta \leq \pi$", fontsize=16)
 
-ax1 = fig.add_subplot(111)
+ax1 = fig1.add_subplot(111)
 ax2 = ax1.twiny()
 ax3 = ax2.twinx()
 
@@ -49,32 +51,83 @@ ax3.set_xlim(min(theta_vals), max(theta_vals))
 ax3.set_ylabel(r"$\mu(\theta)=\cos(\theta)$", fontsize=14)
 ax3.tick_params(direction='in')
 
-fig.legend()
+fig1.legend()
 
-fig.tight_layout()
-
-fig.savefig("expected_probability")
-# plt.show()
-
-plt.clf()
-
-x, y = [], []
-with open("data2.csv", "r") as file:
-    lines = file.readlines()
-    for line in lines:
-        vals = [np.float64(s) for s in line.strip().split(',')]
-        x.append(vals[0])
-        y.append(vals[1])
-    x = np.array(x)
-    y = np.array(y)
+fig1.savefig("expected_probability")
 
 
 def p1(x): return (2/np.pi)*(np.sin(x)**2)
 def p2(x): return (0.375)*(1+x**2)
-def p3(x): return (8/(3*np.pi))*(np.sin(x)**4)
+def p3(x): return (0.375)*(1+(np.cos(x)**2)) * np.sin(x)
 
 
-plt.plot(x, y, 'ko', ls='none', markersize=1, label="Distribution")
-plt.plot(x, p2(x), 'r--', label="Model")
-plt.legend()
+def read_data(filename):
+    x, y = [], []
+    with open(filename, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            vals = [np.float64(s) for s in line.strip().split(',')]
+            x.append(vals[0])
+            y.append(vals[1])
+        x = np.array(x)
+        y = np.array(y)
+    return x, y
+
+
+yticks = np.arange(0.25, 0.875, 0.125)
+
+x, y = read_data('data1.csv')
+
+fig2 = plt.figure(figsize=(12, 12))
+ax1 = fig2.add_subplot(111)
+
+ax1.plot(x, y, 'ko', ls='none', markersize=1, label='Distribution')
+ax1.plot(x, p2(x), 'r--', label='Model')
+ax1.set_xlim(min(x), max(x))
+ax1.set_ylim(None, max(y))
+ax1.set_title('Rejection Method')
+ax1.set_xlabel(r'$\mu$')
+ax1.set_ylabel(r'$P(\mu)$')
+ax1.tick_params(direction='in')
+ax1.set_yticks(yticks, [r'$\frac{1}{4}$', r'$\frac{3}{8}$',
+                        r'$\frac{1}{2}$', r'$\frac{5}{8}$', r'$\frac{3}{4}$'])
+
+fig = plt.figure(figsize=(20, 50))
+
+fig.suptitle("Monte-Carlo methods, N=1000000 & M=1000")
+
+gs = gridspec.GridSpec(2, 1)
+
+ax1 = fig.add_subplot(gs[0, 0])
+ax2 = fig.add_subplot(gs[1, 0])
+
+ax1.plot(x, y, 'ko', ls='none', markersize=1, label='Distribution')
+ax1.plot(x, p2(x), 'r--', label='Model')
+ax1.set_xlim(min(x), max(x))
+ax1.set_ylim(None, max(y))
+ax1.set_title('Rejection Method')
+ax1.set_xlabel(r'$\mu$')
+ax1.set_ylabel(r'$P(\mu)$')
+ax1.tick_params(direction='in')
+ax1.set_yticks(yticks)
+ax1.set_yticklabels([r'$\frac{1}{4}$', r'$\frac{3}{8}$',
+                    r'$\frac{1}{2}$', r'$\frac{5}{8}$', r'$\frac{3}{4}$'])
+
+x, y = read_data('data2.csv')
+
+ax2.plot(x, y, 'ko', ls='none', markersize=1, label="Distribution")
+ax2.plot(x, p2(x), 'r--', label="Model")
+ax2.set_xlim(min(x), max(x))
+ax2.set_ylim(None, max(y))
+ax2.set_title('Importance Sampling')
+ax2.set_xlabel(r'$\mu$')
+ax2.set_ylabel(r'$P(\mu)$')
+ax2.tick_params(direction='in')
+ax2.set_yticks(yticks)
+ax2.set_yticklabels([r'$\frac{1}{4}$', r'$\frac{3}{8}$',
+                    r'$\frac{1}{2}$', r'$\frac{5}{8}$', r'$\frac{3}{4}$'])
+
+ax1.legend()
+ax2.legend()
+
 plt.show()
